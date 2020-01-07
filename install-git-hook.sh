9561756301875014cb39c202e75b7439cb62c477
geewee <gustav@bambuu.dk>
2020-01-07 17:13:16 +0100

    pre-commit hook from curl

--- install-git-hook.sh
+++ install-git-hook.sh
@@ -13,7 +13,7 @@ outFile="./resharper-cli.tar.gz"
 gitResharperFolder="./.git/hooks/resharper"
 preCommitFile="./.git/hooks/pre-commit"
 cliUrl="https://download-cf.jetbrains.com/resharper/ReSharperUltimate.2019.3.1/JetBrains.ReSharper.CommandLineTools.Unix.2019.3.1.tar.gz"
-
+preCommitHookUrl="https://raw.githubusercontent.com/GeeWee/reshaper-pre-commit-hook/master/pre-commit-hook.sh"
 
 
 echo "Fetching Resharper CLI tools"
@@ -26,40 +26,7 @@ echo "Extracting into ${gitResharperFolder}"
 tar -xf "./${outFile}" -C ${gitResharperFolder}
 
 echo "Adding pre-commit hook"
-# The pre-commit hook
-curl -s 
-
-cat > ${preCommitFile} <<'EOL'
-#!/usr/bin/env bash
-# Exit on error
-# exit when any command fails
-set -e
-set -o pipefail
-
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' ERR
-
-# - Actual script
-STAGED_FILES=`git diff --name-only --cached --diff-filter=d`
-echo "Staged files ${STAGED_FILES}"
-
-# Build edit string, by replacing newlines with semicolons.
-# --diff-filter=d only filters files that are not deleted, which means we won't have trouble adding them afterwards
-INCLUDE_STRING=`git diff --name-only --cached --diff-filter=d | sed ':a;N;$!ba;s/\n/;/g'`
-echo "Include string: $INCLUDE_STRING"
-
-# Edit your project files here
-echo "Formatting files..."
-sh ./.git/hooks/resharper/cleanupcode.sh --profile="Built-in: Reformat Code" ./OAI.sln --include="$INCLUDE_STRING"
-
-# Restage files
-echo "Restaging files: $STAGED_FILES"
-echo $STAGED_FILES | xargs -t -l git add
-
-echo "pre-commit hook finished"
-EOL
+curl -s ${preCommitHookUrl} > ${preCommitFile}
 
 echo "Marking as executable"
 chmod u+x ${preCommitFile}
